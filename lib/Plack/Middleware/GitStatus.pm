1bc5e89e8baaa89dd742cfb2075ca3b2f0d5dee8
y_uuki <yuki.tsubo@gmail.com>
2013-02-12 21:28:56 +0900

    initial commit

---
+++ lib/Plack/Middleware/GitStatus.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::GitStatus;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::GitStatus -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::GitStatus;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::GitStatus is
+
+=head1 AUTHOR
+
+Yuuki Tsubouchi E<lt>yuuki@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
